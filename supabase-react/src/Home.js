750399a2d1caae09989d77efbf35d377e6be2813
Noah Fontenette <noahfontenette@outlook.com>
2022-11-30 22:21:15 -0600

    0.0.001

---
+++ supabase-react/src/Home.js
@@ -0,0 +1,10 @@
+import './index.css'
+import Navbar from './Navbar'
+
+export default function Home() {
+    return (
+        <div className="App">
+            <Navbar />
+        </div>
+    )
+}
---
+++ supabase-react/src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <div className="nav">
+            <div className="navlinks">
+                <a href="/" className="navlink">Home</a>
+                <a href="/" className="navlink">About</a>
+                <a href="/" className="navlink">Projects</a>
+                <a href="/" className="navlink">Contact</a>
+            </div>
+        </div>
+    );
+}
+ 
+export default Navbar;
